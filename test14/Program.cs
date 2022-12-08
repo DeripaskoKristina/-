e629f72ee760e566ae0831b474a60bc2d85862dd
DeripaskoKristina <kristallik21@mail.ru>
2022-12-09 00:37:57 +1000

    4 готово

--- test14/Program.cs
+++ test14/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите числа");
+
+int number = int.Parse(Console.ReadLine());
+
+
+for ( int i = 1; i <= number; i ++ )
+{
+    if (i % 2 == 0) Console.WriteLine ($"{number} -> {i}");    
+}
