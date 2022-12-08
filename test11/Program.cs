245a3c19ed0fc46189d713948b935b4e9620b541
DeripaskoKristina <kristallik21@mail.ru>
2022-12-08 22:29:31 +1000

    1 готово

--- test11/Program.cs
+++ test11/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int x = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int y = int.Parse (Console.ReadLine());
+if (x > y)
+{
+Console.WriteLine("Максимальное число" );
+Console.WriteLine(x);
+}
+else
+{
+Console.WriteLine("Максимальное число", y);
+Console.WriteLine(y);
+}
